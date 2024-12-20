e15ed6da5062225ff2360aa25fa8e98bda35c71f
SirvavialTAG <alexyudin04@mail.ru>
2024-12-21 03:10:43 +1000

    Добавлен каталог с функционалом сортировки, пагинации и фильтрации. Реализована часть логики кода, работающего с API (требуется доработка)

--- webbin/settings.py
+++ webbin/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
